b7bc1ab68ed1a489fe535d4355263aa43401c9ef
chetanmenaria00 <chetanmenaria900@gmail.com>
2024-08-17 20:50:39 +0530

    health route

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(cors("*"));
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", protect, taskRoutes);
+// Health
+app.get("health", (req, res) => {
+  res.status(200).json({ code: 200, message: "Healthy" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
